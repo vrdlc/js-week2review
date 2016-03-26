bab00594a0c1ecc2f5e9cabbbcba680608207a69
vrdlc <virgilioramon.delacruz@gmail.com>
2016-03-25 17:50:18 -0700

    fixed pipe filter

--- app/details.pipe.ts
+++ app/details.pipe.ts
@@ -3,19 +3,19 @@ import { Food } from './food.model';
 
 @Pipe({
   name: "details",
-  pure: false
+  pure: true
 })
 
 export class DetailsPipe implements PipeTransform {
   transform(input: Food[], args) {
     var caloriesInFood = args[0];
-    if(caloriesInFood === "unhealthy") {
+    if(caloriesInFood === "notHealthy") {
       return input.filter((food) => {
         return food.foodCalories > 300;
       });
     } else if (caloriesInFood === "healthy") {
       return input.filter((food) => {
-        return food.foodCalories < 300;
+        return food.foodCalories <= 300;
       });
     } else if (caloriesInFood === "nailedIt") {
       return input.filter((food) => {
